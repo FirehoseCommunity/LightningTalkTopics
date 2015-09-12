ad3b64af41218082b15e3f07e2e6fb3e8c404453
Colin Rubbert <colinrubbert@gmail.com>
2015-09-11 19:20:48 -0500

    Update master branch

--- db/schema.rb
+++ db/schema.rb
@@ -54,6 +54,7 @@ ActiveRecord::Schema.define(version: 20150911024655) do
     t.integer  "talk_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
 
 end

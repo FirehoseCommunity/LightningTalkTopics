05e3b9d02ebd5e66534aef69a5bbdff77c2b13b3
Jonathan Mast <jon@padorpers.com>
2015-09-22 18:32:37 -0400

    Remove unused columns from talks
    Also update seed data to no longer reference them.

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,18 +18,18 @@ User.create([
   ])
 
 # Create Talks
-# Example: Talk.create([{topic: "", description: "", is_assigned: [true/false(false by default)], user_id: integer, assigned_to: ""(nil by default), speak_date: date (nil by default) }])
+# Example: Talk.create([{topic: "", description: "", user_id: integer, assignee_id: integer, speak_date: date (nil by default) }])
 Talk.create([
-  {topic: "Continuous Integration", description: "What is Continuous Integration? How does it work? Travis CI, CodeShip, etc?", is_assigned: false, user_id: 1, assigned_to: nil, speak_date: nil },
-  {topic: "Illustrator for Developers", description: "How to quickly get started with Illustrator and apply it to your workflow.", is_assigned: false, user_id: 1, assigned_to: nil, speak_date: nil },
-  {topic: "Vim: It's Good", description: "Vim, a command line editor. Why you should use it? How you should use it? Advantages/Disadvantages?", is_assigned: false, user_id: 1, assigned_to: nil, speak_date: nil },
-  {topic: "Building a Commenting System in Jekyll", description: "Want readers input on your Jekyll blog? Implement a comments section.", is_assigned: false, user_id: 1, assigned_to: nil, speak_date: nil },
-  {topic: "Debugging Heroku", description: "Cover the common errors, gotchas and methodologies to solve the problem.", is_assigned: false, user_id: 2, assigned_to: nil, speak_date: nil },
-  {topic: "Getting Started With Elasticsearch", description: "Real-time search and data analytics.", is_assigned: false, user_id: 2, assigned_to: nil, speak_date: nil },
-  {topic: "Real-time search and data analytics.", description: "Supercharge your TDD with RSpec.", is_assigned: false, user_id: 2, assigned_to: nil, speak_date: nil },
-  {topic: "HTTParty", description: "Making HTTP fun again. Consume restful web services with ease.", is_assigned: false, user_id: 3, assigned_to: nil, speak_date: nil },
-  {topic: "Redis Getting Started", description: "Exploring data structure servers and how to apply them to our applications.", is_assigned: false, user_id: 3, assigned_to: nil, speak_date: nil },
-  {topic: "Stripe Integration", description: "Integrate simple payment processing into your application.", is_assigned: false, user_id: 3, assigned_to: nil, speak_date: nil },
-  {topic: "Getting Started with Vagrant", description: "Learn the ins and outs of utilizing your Vagrant environment to the fullest.", is_assigned: false, user_id: 4, assigned_to: nil, speak_date: nil },
-  {topic: "Sidekiq", description: "Simple, efficient background processing for Ruby.", is_assigned: false, user_id: 4, assigned_to: nil, speak_date: nil }
+  {topic: "Continuous Integration", description: "What is Continuous Integration? How does it work? Travis CI, CodeShip, etc?", user_id: 1, speak_date: nil },
+  {topic: "Illustrator for Developers", description: "How to quickly get started with Illustrator and apply it to your workflow.", user_id: 1, speak_date: nil },
+  {topic: "Vim: It's Good", description: "Vim, a command line editor. Why you should use it? How you should use it? Advantages/Disadvantages?", user_id: 1, speak_date: nil },
+  {topic: "Building a Commenting System in Jekyll", description: "Want readers input on your Jekyll blog? Implement a comments section.", user_id: 1, speak_date: nil },
+  {topic: "Debugging Heroku", description: "Cover the common errors, gotchas and methodologies to solve the problem.", user_id: 2, speak_date: nil },
+  {topic: "Getting Started With Elasticsearch", description: "Real-time search and data analytics.", user_id: 2, speak_date: nil },
+  {topic: "Real-time search and data analytics.", description: "Supercharge your TDD with RSpec.", user_id: 2, speak_date: nil },
+  {topic: "HTTParty", description: "Making HTTP fun again. Consume restful web services with ease.", user_id: 3, speak_date: nil },
+  {topic: "Redis Getting Started", description: "Exploring data structure servers and how to apply them to our applications.", user_id: 3, speak_date: nil },
+  {topic: "Stripe Integration", description: "Integrate simple payment processing into your application.", user_id: 3, speak_date: nil },
+  {topic: "Getting Started with Vagrant", description: "Learn the ins and outs of utilizing your Vagrant environment to the fullest.", user_id: 4, speak_date: nil },
+  {topic: "Sidekiq", description: "Simple, efficient background processing for Ruby.", user_id: 4, speak_date: nil }
   ])

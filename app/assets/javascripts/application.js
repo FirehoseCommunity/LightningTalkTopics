164f0abf6a541f0ac12db1a2b82fbae22063479b
Takehiro Mouri <takehiromouri@gmail.com>
2015-12-06 08:39:18 +0000

    Added ckeditor

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,7 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require ckeditor/init
 //= require jquery
 //= require bootstrap-sprockets
 //= require jquery_ujs
